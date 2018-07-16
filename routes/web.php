e3b79fad39a495651e9299a37e9ab067343bc6cd
lgyer <lgy330395508@gmail.com>
2018-07-16 14:46:35 +0800

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Route::group(['middleware' => 'auth'], function (){
 	Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
@@ -35,8 +36,8 @@ Route::group(['middleware' => 'auth'], function (){
 		Route::get('user_addresses/{user_address}', 'UserAddressController@edit')->name('user_addresses.edit');
 		Route::put('user_addresses/{user_address}', 'UserAddressController@update')->name('user_addresses.update');
 		Route::delete('user_addresses/{user_address}', 'UserAddressController@destroy')->name('user_addresses.destroy');
-		Route::get('products', 'ProductsController@index')->name('products.index');
-		Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+		Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+		Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 		Route::get('/test', function () {
 			echo '邮箱验证通过';
 		});
