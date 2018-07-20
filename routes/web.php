6dfb1500277974a28b423a61071deba95f23e1a1
lgyer <lgy330395508@gmail.com>
2018-07-20 15:11:41 +0800

    :sparkles: 购物车页面
    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::group(['middleware' => 'auth'], function (){
 		Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 		Route::post('cart', 'CartController@add')->name('cart.add');
 		Route::get('cart', 'CartController@index')->name('cart.index');
+		Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 		Route::get('/test', function () {
 			echo '邮箱验证通过';
 		});
